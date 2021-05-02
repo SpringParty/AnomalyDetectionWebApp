89a51768ec77e67ac5c0c0521465e23bb8785254
Spring_Party <Yuval.e@hotmail.com>
2021-05-02 17:28:18 +0300

    Added more REST requests

--- server.js
+++ server.js
@@ -5,20 +5,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { deepStrictEqual } = require('assert');
 
+
 const app = express();
 const port = 9876;
 const modelsStatusFile = path.join(__dirname, 'models', 'modelsStatus.csv');
 
+const readyStatus = "ready";
+const pendingStatus = "pending";
+
 try {    
     // if models file exists
     if (fs.existsSync(modelsStatusFile)) {
-        console.log("modelsStatus.csv already exists");
+        console.log(`${modelsStatusFile} already exists`);
     // if models file does not exist
     } else {
         // try to create the file
         fs.writeFile(modelsStatusFile, 'model_id,upload_time,status\n', 'utf8', function(err) {
             if (err) return console.log(err);
-            console.log("Created modelsStatus.csv");
+            console.log(`Created ${modelsStatusFile}`);
         });
     }
 } catch (err) {
@@ -37,7 +41,7 @@ app.get('/api/model', (req, res) => {
         csv()
             .fromFile(modelsStatusFile)
             .then((modelStatusData) => {
-                var msgSent = false;
+                let msgSent = false;
                 // for each row in modelsStatus.csv file
                 for (let data of modelStatusData) {
                     // if row's model_id equals to the given parameter
@@ -115,11 +119,78 @@ app.delete('/api/model', (req, res) => {
 });
 
 
-app.post('/api/world', (req, res) => {
-    console.log(req.body);
-    res.send(
-        `I received your POST requrest. This is what you sent me: ${req.body.post}`,        
-    );
+app.post('/api/model', (req, res) => {    
+    const modelType = req.query["model_type"];
+    
+    let modelID = 5467;
+    
+    let uploadedTime = new Date();        
+    let uploadedTimeZone = uploadedTime.toString().match(/([-\+][0-9]+)\s/)[1];
+    uploadedTimeZone = [uploadedTimeZone.slice(0, 3), ".", uploadedTimeZone.slice(3)].join('');
+
+    uploadedTime = `${uploadedTime.getFullYear().toString().padStart(4, '0')}-` + 
+                   `${(uploadedTime.getMonth()+1).toString().padStart(2, '0')}-` +
+                   `${uploadedTime.getDate().toString().padStart(2, '0')}T` +
+                   `${uploadedTime.getHours().toString().padStart(2, '0')}:` +
+                   `${uploadedTime.getMinutes().toString().padStart(2, '0')}:` +
+                   `${uploadedTime.getSeconds().toString().padStart(2, '0')}` +
+                   `${uploadedTimeZone}`;
+
+    if (modelType !== "regression" && modelType !== "hybrid") {
+        res.status(404).send(`Illegal model type`);
+        return;
+    } else {
+        // append pending model into modelStatus.csv file
+        fs.appendFile(modelsStatusFile, `${modelID},${uploadedTime},${pendingStatus}\n`, function(err) {
+            if (err) console.error(err);
+            console.log('Entered new record into modelStatus.csv');
+        });
+
+        let correlatedData = null;
+
+        if (modelType === "regression") {
+            console.log("regression model type");
+
+            //TODO: call dll of regression algorithm, and insert correlatedFeatures data into correlatedData
+        } else {
+            console.log("hybrid model type");
+
+            //TODO: call dll of hybrid algorithm, and insert correlatedFeatures data into correlatedData
+        }
+        
+        // create .csv file for the processed model
+        // let modelFile = path.join(__dirname, 'models', req.body.model_id + ".csv");
+        // fs.writeFile(modelFile, correlatedData, 'utf8', function(err) {
+        //     if (err) return console.log(err);
+        //     console.log(`Created ${modelFile}`);
+        // });
+
+        // set model status as ready
+        fs.readFile(modelsStatusFile, 'utf8', function(err, data) {
+            if (!err) {
+                // get lines of the .csv file, and split them by ','
+                let lines = data.split('\n')
+                let linesArr = lines.map(line=>line.split(','));
+                
+                for (line of linesArr) {
+                    // if the current row is related to the current modelID
+                    if (line[0] == modelID) {                        
+                        // change the status of the model
+                        line[2] = readyStatus;                        
+                        break;
+                    }
+                }
+                // upload the changes into modelsStatus.csv file
+                let output = linesArr.join("\n");
+                fs.writeFileSync(modelsStatusFile, output);                
+            } else {
+                res.status(404).send();
+            }            
+        }, res, modelID, readyStatus);
+    }
+
+    console.log(`model ${modelID} was created`);
+    res.status(200).send(`model ${modelID} was created`);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
