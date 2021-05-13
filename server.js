49a556b3a50ef2e0931e37b8142dec3368d8b3f0
Spring_Party <Yuval.e@hotmail.com>
2021-05-13 20:42:15 +0300

    server and client on same port

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const bodyParser = require("body-parser");
 const fileupload = require("express-fileupload");
 const Detector = require("./Detector.js");
 
-const modelsDir = "C:\\Users\\user\\Documents\\University\\Test\\models";
-// const modelsDir = path.join(__dirname, "models");
+// const modelsDir = "C:\\Users\\user\\Documents\\University\\Test\\models";
+const modelsDir = path.join(__dirname, "models");
 // if models folder does not exist
 try {
   if (!fs.existsSync(modelsDir)) {
@@ -17,23 +17,23 @@ try {
   console.error(err);
 }
 
-const PORT = 5000;
+const PORT = 8080;
 const app = express();
-// const buildPath = __dirname + "/build";
-// app.use(express.static(buildPath));
+const buildPath = __dirname + "/build";
+app.use(express.static(buildPath));
 
-// const corsOptions = {
-// origin: "http://localhost:" + PORT,
-// };
-// app.use(cors(corsOptions));
+const corsOptions = {
+origin: "http://localhost:" + PORT,
+};
+app.use(cors(corsOptions));
 app.use(fileupload());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// // homepage
-// app.get("/", (req, res) => {
-//   res.send(buildPath + "index.html");
-// });
+// homepage
+app.get("/", (req, res) => {
+  res.send(buildPath + "index.html");
+});
 
 // POST command of detect
 app.post("/api/detect", (req, res) => {
